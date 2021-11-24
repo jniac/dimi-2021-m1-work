25679bbd9331e1d0d84e74a391c0f69a23538cb0
Amélie Marmot <amelisa.marmot@gmail.com>
2021-11-24 16:04:01 +0100

    re

--- works/AmelieMarmot/langton-ant/js/langton-ant.js
+++ works/AmelieMarmot/langton-ant/js/langton-ant.js
@@ -48,7 +48,7 @@ const move = () => {
   //   et remettre le pixel en blanc ("#ffffff")
   if (color === '#ffffff') {
     turnLeft()    
-    setPixel(x, y, 'turquoise')
+    setPixel(x, y, '#483D8B')
   } else {
     turnRight()
     setPixel(x, y, '#ffffff')
