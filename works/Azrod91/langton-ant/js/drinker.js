e7588ca4e3d2f3e2672d04ec565aec8dec4a4da9
Azrod91 <59173631+Azrod91@users.noreply.github.com>
2021-11-24 17:04:14 +0100

    comit position aleatoire drinker

---
+++ works/Azrod91/langton-ant/js/drinker.js
@@ -0,0 +1,79 @@
+import { canvas, getPixel, setPixel } from '../../../common/canvas.js'
+
+const RIGHT = 0
+const BOTTOM = 1
+const LEFT = 2
+const UP = 3
+
+let x = 20
+let y = 20
+let orientation = RIGHT
+
+const setRandonmPosition = ()=>{
+  x = Math.floor(Math.random()*canvas.width)
+  y = Math.floor(Math.random()*canvas.height)
+}
+
+const moveForward = () => {
+  if (orientation === RIGHT) {
+    x = x + 1
+  } else if (orientation === BOTTOM) {
+    y = y + 1
+  } else if (orientation === LEFT) {
+    x = x - 1
+  } else if (orientation === UP) {
+    y = y - 1
+  }
+}
+
+const turnRight = () => {
+  orientation = orientation + 1
+  if (orientation === 4) {
+    orientation = 0
+  }
+}
+
+const turnLeft = () => {
+  orientation = orientation - 1
+  if (orientation === -1) {
+    orientation = 3
+  }
+}
+
+const move = () => {
+  
+  const color = getPixel(x, y)
+
+  // Algorithme de la fourmi de langton :
+  // Pour une couleur précise (ici le blanc "#ffffff") :
+  //   tourner à gauche ("turnLeft()")
+  //   et peindre le pixel en un couleur sympa ("turquoise")
+  // sinon :
+  //   tourner à droite ("turnRight()")
+  //   et remettre le pixel en blanc ("#ffffff")
+  if (color === '#ffffff') {
+    turnLeft()    
+    setPixel(x, y, 'turquoise')
+  } else {
+    turnRight()
+    setPixel(x, y, '#ffffff')
+  }
+  
+  moveForward()
+}
+
+const setPosition = (positionX, positionY) => {
+  x = positionX
+  y = positionY
+}
+
+export {
+  x,
+  y,
+  orientation,
+  move,
+  setPosition,
+  setRandonmPosition,
+
+}
+
