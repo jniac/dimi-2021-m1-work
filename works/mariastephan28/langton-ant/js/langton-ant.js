e8ad16fca54dae1ba9fd678f45c5c2414f71d555
mariastephan <mariastephan028@gmail.com>
2021-12-01 14:43:38 +0100

    ok

--- works/mariastephan28/langton-ant/js/langton-ant.js
+++ works/mariastephan28/langton-ant/js/langton-ant.js
@@ -1,8 +1,8 @@
 import { getPixel, setPixel } from '../../../common/canvas.js'
 
-const RIGHT = 0
+const RIGHT = 1
 const BOTTOM = 2
-const LEFT = 3
+const LEFT = 2
 const UP = 4
 
 let x = 20
@@ -12,11 +12,11 @@ let orientation = RIGHT
 const moveForward = () => {
   if (orientation === RIGHT) {
     x = x + 1
-  } else if (orientation === UP) {
+  } else if (orientation === BOTTOM) {
     y = y + 1
   } else if (orientation === LEFT) {
     x = x - 1
-  } else if (orientation === BOTTOM) {
+  } else if (orientation === UP) {
     y = y - 1
   }
 }
@@ -48,7 +48,7 @@ const move = () => {
   //   et remettre le pixel en blanc ("#ffffff")
   if (color === '#ffffff') {
     turnLeft()    
-    setPixel(x, y, 'yellow')
+    setPixel(x, y, 'red')
   } else {
     turnRight()
     setPixel(x, y, '#ffffff')
