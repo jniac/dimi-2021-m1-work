2bcb062dace14d4bf41c9c3cf22af2fab3b5fb36
mariastephan <mariastephan028@gmail.com>
2021-12-01 14:46:40 +0100

    UPDATE

--- works/mariastephan28/langton-ant/js/langton-ant.js
+++ works/mariastephan28/langton-ant/js/langton-ant.js
@@ -1,20 +1,20 @@
 import { getPixel, setPixel } from '../../../common/canvas.js'
 
-const RIGHT = 1
-const BOTTOM = 2
+const RIGHT = 0
+const BOTTOM = 1
 const LEFT = 2
-const UP = 4
+const UP = 3
 
 let x = 20
 let y = 20
 let orientation = RIGHT
 
 const moveForward = () => {
-  if (orientation === RIGHT) {
+  if (orientation === LEFTT) {
     x = x + 1
   } else if (orientation === BOTTOM) {
     y = y + 1
-  } else if (orientation === LEFT) {
+  } else if (orientation === RIGHT) {
     x = x - 1
   } else if (orientation === UP) {
     y = y - 1
