cd2c0c216556c40729417cc432d0eaf44b588e0f
nejiaben <nejia.benhamida96@gmail.com>
2021-12-01 15:55:10 +0100

    fourmi

--- works/walkers/walkers.js
+++ works/walkers/walkers.js
@@ -2,6 +2,7 @@ import { canvas } from '../common/canvas.js'
 import * as jniacWalker from '../jniac/langton-ant/js/my-walker.js'
 import * as mariaWalker from '../mariastephan28/langton-ant/js/my-walker.js'
 import * as maelcmpWalker from '../MaelCmp/langton-ant/js/my-walker.js'
+import * as nejiabenWalker from '../Nejiaben/langton-ant/js/my-walker.js'
 
 export const allWalkers = [
   jniacWalker,
