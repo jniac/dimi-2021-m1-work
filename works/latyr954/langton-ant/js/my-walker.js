08e7a95f1f92f06e59e1b1a81a9c029df1c64f31
latyr954 <latyr95@icloud.com>
2021-12-01 16:16:15 +0100

    latyr walker

---
+++ works/latyr954/langton-ant/js/my-walker.js
@@ -0,0 +1,60 @@
+import { clampPosition, setPixel } from '../../../common/canvas.js'
+
+const RIGHT = 0
+const BOTTOM = 1
+const LEFT = 2
+const UP = 3
+
+const getRandomColor = () => {
+  const colors = ['ffd700','ff8000']
+  const color = colors[colorIndex]
+  colorIndex = colorIndex + 1
+  if (colorIndex >= colors.length) {
+    colorIndex = 0
+  }
+  return color
+}
+
+let x = 20
+let y = 20
+let orientation = RIGHT
+let color = getRandomColor()
+
+const setPosition = (positionX, positionY) => {
+  x = positionX
+  y = positionY
+}
+
+const moveForward = () => {
+  if (orientation === RIGHT) {
+    x = x + 1
+  } else if (orientation === BOTTOM) {
+    y = y + 1
+  } else if (orientation === LEFT) {
+    x = x - 1
+  } else if (orientation === UP) {
+    y = y - 1
+  }
+
+  const p = clampPosition(x, y)
+  x = p.x
+  y = p.y
+}
+
+const move = () => {
+  setPixel(x, y, color)
+
+  const shouldChange = Math.random() < 0.1
+  if (shouldChange) {
+    color = getRandomColor()
+    orientation = Math.floor(Math.random() * 4)
+  }
+
+  moveForward()
+}
+
+export {
+  x, y,
+  setPosition,
+  move,
+}
