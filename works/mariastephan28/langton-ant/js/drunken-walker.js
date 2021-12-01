fee31ef1658cb221628caadff8c536d2173c67f6
mariastephan <mariastephan028@gmail.com>
2021-12-01 15:18:10 +0100

    ok

--- works/mariastephan28/langton-ant/js/drunken-walker.js
+++ works/mariastephan28/langton-ant/js/drunken-walker.js
@@ -27,9 +27,9 @@ const moveForward = () => {
 
 const getRandomColor = () => {
   if (Math.random() < 0.5) {
-    return 'goldenrod'
+    return '#b31b1b'
   }
-  return 'red'
+  return '#002e63'
 }
 
 export const move = () => {
