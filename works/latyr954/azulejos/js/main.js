f059e08a2cca079ed92ea9bd6d79b3b15b7c675b
latyr954 <latyr95@icloud.com>
2021-12-01 23:55:19 +0100

    s

--- works/latyr954/azulejos/js/main.js
+++ works/latyr954/azulejos/js/main.js
@@ -1,49 +1,35 @@
-
 const getRandomColor = () => {
-  const r = Math.random()
-  if (r < 0.5) {
-    if (Math.random() < 0.5) {
-      return '#D8BFD8'//chardon//
-    }
-    return '#6A5ACD'//bleu ardoise//
-  }
-  if (r < 0.7) {
-    return '#E6E6FA'//lavande//
-  }
-  if (r < 0.9) {
-    return '#4B0082'//indingo//
-  }
-  return '#9370DB'//moyen violet//
+    let tab = ['#003d69','#006dd5','#ff4849','#ffd03a','#f8ffb0']
+    const randomColor = tab[Math.floor(Math.random() * tab.length)];
+    return randomColor
 }
 
 const createType1 = () => {
-  const source = document.querySelector('#library .type1')
-  const clone = source.cloneNode(true)
-  clone.style.backgroundColor = getRandomColor()
-  clone.querySelector('.layer1').style.backgroundColor = getRandomColor()
-  clone.querySelector('.layer2').style.backgroundColor = getRandomColor()
-  clone.querySelector('.layer3').style.backgroundColor = getRandomColor()
-  return clone
+    const source = document.querySelector('#library .type1')
+    const clone = source.cloneNode(true)
+    clone.style.backgroundColor = getRandomColor()
+    clone.querySelector('.layer1').style.backgroundColor = getRandomColor()
+    clone.querySelector('.layer2').style.backgroundColor = getRandomColor()
+    clone.querySelector('.layer3').style.backgroundColor = getRandomColor()
+    return clone
 }
 
 const createTile = () => {
-  const div = document.createElement('div')
-  div.className = 'tile'
-  document.querySelector('main').append(div)
-
-  div.append(createType1())
-
-  return div
-}
-
-const createGrid = (col, row) => {
-  for (let y = 0; y < row; y += 1) {
-    for (let x = 0; x < col; x += 1) {
-      const tile = createTile()
-      tile.style.left = `${x * 120}px`
-      tile.style.top = `${y * 120}px`
+    const div = document.createElement('div')
+    div.className = 'tile'
+    document.querySelector('main').append(div)
+    div.append(createType1())
+    return div
+  }
+  
+  const createGrid = (col, row) => {
+    for (let y = 0; y < row; y += 1) {
+      for (let x = 0; x < col; x += 1) {
+        const tile = createTile()
+        tile.style.left = `${x * 180}px`
+        tile.style.top = `${y * 180}px`
+      }
     }
   }
-}
-
-createGrid(4, 3)
+  
+  createGrid(4, 3)
