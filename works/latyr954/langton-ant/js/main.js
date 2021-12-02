107b619d8bbc494c90585d748e509c52e187828b
latyr954 <latyr95@icloud.com>
2021-12-02 10:11:07 +0100

    ajout latyr

--- works/latyr954/langton-ant/js/main.js
+++ works/latyr954/langton-ant/js/main.js
@@ -1,6 +1,5 @@
 import { resizeCanvas } from '../../../common/canvas.js'
 import * as ant from './langton-ant.js'
-import * as azulejos from './azulejos.js'
 import * as drunken from './drunken-walker.js'
 import * as myWalker from './my-walker.js'
 
@@ -15,6 +14,8 @@ let frame = 0
 const update = () => {
   for (let i = 0; i < speed; i++) {
     ant.move()
+    drunken.move()
+    myWalker.move()
     frame += 1
   }
   document.querySelector('#frame').innerHTML = `#${frame.toString().padStart(5, '0')}@x${speed}`
