caab450c44899f7a9227e9078ff2d20d3e9bb954
nejiaben <nejia.benhamida96@gmail.com>
2021-12-31 13:09:06 +0100

    n

--- works/Nejiaben/langton-ant/js/langton-ant.js
+++ works/Nejiaben/langton-ant/js/langton-ant.js
@@ -53,6 +53,28 @@ const move = () => {
     turnRight()
     setPixel(x, y, '#ffffff')
   }
+
+  const move = () => {
+  
+    const color = getPixel(x, y)
+  
+     // Algorithme de la fourmi de langton :
+    // Pour une couleur précise (ici le blanc "#ffffff") :
+    //   tourner à gauche ("turnLeft()")
+    //   et peindre le pixel en un couleur sympa ("darkslateblue")
+    // sinon :
+    //   tourner à droite ("turnRight()")
+    //   et remettre le pixel en blanc ("#ffffff")
+    if (color === '#191970') {
+      turnLeft()    
+      setPixel(x, y, 'darkslateblue')
+    } else {
+      turnRight()
+      setPixel(x, y, '#191970')
+    }
+    
+    moveForward()
+  }
   
   moveForward()
 }
