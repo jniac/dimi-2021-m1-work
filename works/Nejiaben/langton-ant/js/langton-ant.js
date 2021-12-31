98dd9d81289094da1e8a6c21b6c0ae80417dbd27
nejiaben <nejia.benhamida96@gmail.com>
2021-12-31 09:57:02 +0100

    1

--- works/Nejiaben/langton-ant/js/langton-ant.js
+++ works/Nejiaben/langton-ant/js/langton-ant.js
@@ -14,9 +14,9 @@ const moveForward = () => {
     x = x + 1
   } else if (orientation === BOTTOM) {
     y = y + 1
-  } else if (orientation === UP) {
-    x = x - 1
   } else if (orientation === LEFT) {
+    x = x - 1
+  } else if (orientation === UP) {
     y = y - 1
   }
 }
